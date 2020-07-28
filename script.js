39d6cc87bdd8649c905d21dca7adf105bddbb81e
Emperor of Bluegaria <62402965+Bluegarian-Tech-Archives@users.noreply.github.com>
2020-07-28 14:06:35 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 'use strict';
 
-let notDoneMsg = 'Alert: This website is currently still under development and is not finished yet! PLEASE NO COMPLAINS!! Thank you! ☻'
-
+let notDoneMsg = 'Alert: This website is currently still under development and is not finished yet! PLEASE NO COMPLAINS!! Thank you! ☻';
 alert(notDoneMsg);
+
+function waitLoadBtnScrollTop() {
+    document.getElementById('btnScrollTop').addEventListener('click', function(){
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    })
+}
+
+
+
